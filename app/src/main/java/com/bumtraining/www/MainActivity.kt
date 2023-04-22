14fde44fb2d079b7a4523a8d35c0d7af2b38b35e
Vlada955 <Vladaivanovic030@gmail.com>
2023-04-22 14:35:17 +0200

    First commit

---
+++ app/src/main/java/com/bumtraining/www/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.bumtraining.www
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bumtraining.www.databinding.ActivityMainBinding
+import com.bumtraining.www.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Controller
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+//        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_training -> {
+                    navController.navigate(R.id.trainingFragment)
+                    true
+                }
+                R.id.item_nutrition -> {
+                    navController.navigate(R.id.nutritionFragment)
+                    true
+                }
+                R.id.item_progress -> {
+                    navController.navigate(R.id.progressFragment)
+                    true
+                }
+                R.id.item_chat -> {
+                    navController.navigate(R.id.chatFragment)
+                    true
+                }
+                R.id.item_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/bumtraining/www/fragments/TrainingFragment.kt
@@ -0,0 +1,13 @@
+package com.bumtraining.www.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumtraining.www.R
+import com.bumtraining.www.databinding.ActivityMainBinding
+
+class TrainingFragment : Fragment(R.layout.fragment_training) {
+
+}
